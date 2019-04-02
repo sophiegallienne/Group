9192c4d9d6d9113ae542955a77df27ebd5416407
Lowri Hughes <lowri.milvanahug@gmail.com>
2019-04-02 11:39:57 +0100

    Story Board
    I also added a Navigation controller to the Story board :)
    
    and deleted the old comments

--- GitHub Demo/ViewController.swift
+++ GitHub Demo/ViewController.swift
@@ -5,10 +5,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //testing
-        //Plz work :)
+
     }
     
 }
-
-// can you guys see this?
