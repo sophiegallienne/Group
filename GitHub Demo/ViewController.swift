299b287333130c2a2b0925b771ccaf05686908c1
Sophie Gallienne <sophiegallienne@Sophies-MacBook.lan>
2019-04-06 15:03:06 +0100

    Added start and leaderboard buttons to the opening screen

--- GitHub Demo/ViewController.swift
+++ GitHub Demo/ViewController.swift
@@ -3,9 +3,14 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
 
     }
+    @IBAction func startButton(_ sender: UIButton) {
+    }
     
+    @IBAction func leaderboardButton(_ sender: UIButton) {
+    }
 }
