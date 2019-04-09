85f92225c97b7f6ff46f91d9f2e9a2330791b23e
Sophie Gallienne <sophiegallienne@Sophies-MacBook.lan>
2019-04-09 10:29:37 +0100

    begining to code the segue from the load screen to the detailed view controller

--- GitHub Demo/ViewController.swift
+++ GitHub Demo/ViewController.swift
@@ -2,8 +2,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var characters = Characters()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         
     }
     @IBAction func startButton(_ sender: UIButton) {
@@ -16,11 +19,28 @@ class ViewController: UIViewController {
 
 class SecondViewController: UIViewController {
     
+   
+    
+    imageView.delegate = self
+    
     
 }
 
 override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
     let vc = segue.destination as! SecondViewController
+    vc.annotation = sender as? CustomAnnotation
     
+}
+
+
+
+extension ViewController: UIImageViewDelegate {
+    
+    func imageView(_ imageView: UIimageView, didSelect view: MKAnnotationView) {
+        guard let annotation = view.annotation as? CustomAnnotation else { return }
+        performSegue(withIdentifier: "Next", sender: annotation)
+        imageView.deselectAnnotation(annotation, animated: true)
+
+}
 
 }
