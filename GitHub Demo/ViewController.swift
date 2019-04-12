a6b54f6cc6ad7b708d543b2e6d70b733d955a1f2
Sophie Gallienne <sophiegallienne@Sophies-MacBook.lan>
2019-04-12 10:52:14 +0100

    Revert "Merge branch 'master' of https://github.com/sophiegallienne/Group"
    This reverts commit ba4965ca96185262eaaaca661c2251e06777608f, reversing
    changes made to 6495db67775caae322947ee91d6987688684db8d.

---
+++ GitHub Demo/ViewController.swift
@@ -0,0 +1,15 @@
+import UIKit
+import Firebase
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+    
+//The character would change as the view controller changes, depending on the decisions the user makes.
+    @IBOutlet weak var starterCharacter: UIImageView!
+}
+
