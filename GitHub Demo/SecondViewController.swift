f73224f74b6eb2b4d45e848e75b5627b06e8a5a8
Nicole Chivers (s5113508) <s5113508@mac38581.mac.bournemouth.ac.uk>
2019-04-29 09:58:10 +0100

    2ndVC changes

--- GitHub Demo/SecondViewController.swift
+++ GitHub Demo/SecondViewController.swift
@@ -1,4 +1,19 @@
-import Foundation
+import UIKit
+
+protocol SecondViewControllerDelegate {
+    
+}
+
+class SecondViewController: UIViewController {
+    
+    var delegate: SecondViewControllerDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
 
 
 extension ViewController: SecondViewController {
