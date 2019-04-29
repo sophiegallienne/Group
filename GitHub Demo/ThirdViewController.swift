43bcb2c1810b048c2ca626c0eece77c3bcbfbf30
Sophie Gallienne <sophiegallienne@Sophies-MacBook.local>
2019-04-29 10:18:38 +0100

    deleted comments

--- GitHub Demo/ThirdViewController.swift
+++ GitHub Demo/ThirdViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ThirdViewController.swift
-//  GitHub Demo
-//
-//  Created by Lowri Hughes (s5112822) on 29/04/2019.
-//  Copyright © 2019 Sophie Gallienne. All rights reserved.
-//
-
 import UIKit
 
 protocol ThirdViewControllerDelegate {
