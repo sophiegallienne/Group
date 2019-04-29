a5cba23e0511b0bcd30e4ade5221c3c99eec9b94
Sophie Gallienne <sophiegallienne@Sophies-MacBook.local>
2019-04-29 10:43:26 +0100

    moved 'questions' into its own swift file

--- GitHub Demo/ViewController.swift
+++ GitHub Demo/ViewController.swift
@@ -10,30 +10,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var Question: UILabel!
     @IBOutlet weak var choiceOne: UIButton!
     @IBOutlet weak var choiceTwo: UIButton!
+        
     
-    struct Question {
-        var Question: String!
-        var Answer :[String]
-        var Answer: Int!
-        
-        var Question = [Question]()
-        
-        //lowri pls fill in this bit with the narrative bits (this will fix the error)
-        Question = [Question(""), Answers: "", "", Answer: ""]
-        
-        override func didReciveMemoryWarning(){
-        super.didReciveMemoryWarning()
-        func PickQuestion()
-        
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+        
 
     }
-    
-}
 
 extension ViewController: SecondViewControllerDelegate {
     
