717822c3a8676bafd539a1a011f6cf90f3f93402
Lowri Hughes (s5112822) <s5112822@mac38582.mac.bournemouth.ac.uk>
2019-04-29 10:28:38 +0100

    fourth View Controler

--- GitHub Demo/FourthViewController.swift
+++ GitHub Demo/FourthViewController.swift
@@ -6,6 +6,10 @@ protocol FourthViewControllerDelegate {
 
 class FourthViewController: UIViewController {
     
+    @IBOutlet weak var Background: UIImageView!
+    @IBOutlet weak var Label: UILabel!
+    
+    
     var delegate: FourthViewControllerDelegate?
     
     override func viewDidLoad() {
