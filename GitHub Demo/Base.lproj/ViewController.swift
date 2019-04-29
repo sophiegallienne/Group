59dc53605a637a5435a3dc583cff1f1d402f1e18
Sophie Gallienne <sophiegallienne@Sophies-MacBook.local>
2019-04-29 10:52:06 +0100

    yea na whats goin on

--- GitHub Demo/Base.lproj/ViewController.swift
+++ GitHub Demo/Base.lproj/ViewController.swift
@@ -3,21 +3,6 @@ import Firebase
 
 class ViewController: UIViewController {
     
-    
-    struct Question {
-        var Question: String!
-        var Answer :[String]
-        var Answer: Int!
-        
-        var Question = [Question]()
-        
-        //lowri pls fill in this bit with the narrative bits (this will fix the error)
-        Question = [Question(""), Answers: "", "", Answer: ""]
-        
-        override func.didReciveMemoryWarning()
-        super.didReciveMemoryWarning()
-        
-        func PickQuestion()
         
     }
 
