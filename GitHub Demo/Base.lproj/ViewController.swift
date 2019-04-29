e046de5cdd26b0a93f5af345d26f9092d37bcd0c
Nicole Chivers (s5113508) <s5113508@mac38581.mac.bournemouth.ac.uk>
2019-04-29 10:02:09 +0100

    vc segue changes

--- GitHub Demo/Base.lproj/ViewController.swift
+++ GitHub Demo/Base.lproj/ViewController.swift
@@ -28,6 +28,16 @@ class ViewController: UIViewController {
 
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.destination is ThirdViewController {
+            //its a third view controller
+        }
+        if let vc = segue.destination as? SecondViewController {
+            //its a second view controller
+            vc.annotation = sender as? CustomAnnotation
+        }
+    }
+    
 }
 
 extension ViewController: SecondViewControllerDelegate {
